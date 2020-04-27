a6501fc0808ae0ee1d615331cf208e4ddbfcea10
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-27 13:30:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,12 @@
 def using_push(array, string)
-  array = []
   array.push(string)
+end
+def using_unshift(array, string)
+  array.unshift(string)
+end
+def unsing_pop(array)
+  array.pop()
+end
+def pop_with_args(array)
+  array.pop(-1, -2)
 end
